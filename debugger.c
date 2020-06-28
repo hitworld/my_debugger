f850ce19e20539f58599190f910e1f0c7091ef5a
w4rd3n <543546992@qq.com>
2020-06-28 18:10:11 +0800

    update breakpoint

--- debugger.c
+++ debugger.c
@@ -8,16 +8,30 @@
 #include <sys/wait.h>
 #include "linenoise.h"
 
+struct Debugger_Data {
+    long long break_addrs[0x10];
+    char break_datas[0x10];
+} my_data;
+
 void continue_execution(int pid) {
     int wait_status;
     int options = 0;
 
     ptrace(PTRACE_CONT, pid, NULL, NULL);
     waitpid(pid, &wait_status, options);
+    printf("break");
+    fflush(stdout);
 }
 
-void breakpoint_enable(int pid) {
-    printf("%d", pid);
+void breakpoint_enable(int pid, long long value) {
+    printf("pid: %d, addr: %p\n", pid, (void *)value);
+    fflush(stdout);
+    long data = ptrace(PTRACE_PEEKDATA, pid, value, NULL);
+    my_data.break_addrs[0] = value;
+    my_data.break_datas[0] = (data & 0xff);
+    ptrace(PTRACE_POKEDATA, pid, value, ((data & ~0xff) | 0xcc));
+
+    return;
 }
 
 void handle_command(int pid, char * str) {
@@ -25,14 +39,49 @@ void handle_command(int pid, char * str) {
 
     if (!strcmp(command, "c")) {
         continue_execution(pid);
+        return;
     } else if (!strcmp(command, "b")) {
         char * expr = strtok(NULL, "\n");
-        puts(expr);
-        breakpoint_enable(pid);
+        if(!expr) {
+            printf("Error:b <expr>\n");
+            return;
+        }
+        int len = strlen(expr);
+        long long value = 0;
+        if(strncmp("0x", expr, 2) == 0) {
+            int i = 2;
+            for(;(i < len) && ((expr[i] >= 0x30 && expr[i] <= 0x39) || (expr[i] >= 0x41 && expr[i] <= 0x5A) || (expr[i] >= 0x61 && expr[i] <= 0x7A));i++) {
+                if(expr[i] >= 0x30 && expr[i] <= 0x39) {
+                    value *= 0x10;
+                    value += expr[i] - 0x30;
+                } else if (expr[i] >= 0x41 && expr[i] <= 0x46) {
+                    value *= 0x10;
+                    value += expr[i] - 0x37;
+                } else {
+                    value *= 0x10;
+                    value += expr[i] - 0x57;
+                }
+            }
+            if(i != len) {
+                printf("expr\n");
+            }
+        } else {
+            int i = 0;
+            for(;(i < len) && (expr[i] >= 0x30 && expr[i] <= 0x39);i++) {
+                value *= 10;
+                value += expr[i] - 0x30;
+            }
+            if(i != len) {
+                printf("expr\n");
+            }
+        }
+        breakpoint_enable(pid, value);
+        return;
     } else if (!strcmp(command, "q")) {
         ptrace(PTRACE_KILL, pid, NULL, NULL);
         exit(0);
     }
+    return;
 }
 
 int main(int argc, char * argv[]) {
